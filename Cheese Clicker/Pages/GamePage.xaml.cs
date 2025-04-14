c8ae46a090946ca1e1c84fdf913914e96cdee4c5
Nooble12 <timchitran9@gmail.com>
2025-04-13 22:55:30 -0700

    Added quick keys for GamePage buttons.

--- Cheese Clicker/Pages/GamePage.xaml.cs
+++ Cheese Clicker/Pages/GamePage.xaml.cs
@@ -4,6 +4,7 @@ using Cheese_Clicker.PlayerClasses;
 using Cheese_Clicker.SoundClasses;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Navigation;
 
 namespace Cheese_Clicker.Pages
@@ -28,9 +29,42 @@ namespace Cheese_Clicker.Pages
 
             rewardGenerator = new GenerateReward(player);
             moneyLabelEffect = new MoneyLabelEffect(this);
+
+            //For keyboard related presses
+            this.Focusable = true;
+            this.Focus();
+            this.KeyDown += Keyboard_KeyDown;
         }
 
         private void CheeseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ActivateCheeseButton();
+        }
+
+        private void Keyboard_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Space:
+                    ActivateCheeseButton();
+                break;
+                    
+                case Key.D1:
+                    DisplayShopPage();
+                break;
+
+                case Key.D2:
+                    DisplayInventoryPage();
+                break;
+
+                case Key.D3:
+                    DisplaySettingsPage();
+                break;
+            }
+        }
+
+        //On cheese button click or space bar keybind
+        private void ActivateCheeseButton()
         {
             Reward reward = rewardGenerator.GetReward();
             player.statistics.AddMoney(reward.moneyGained);
@@ -44,12 +78,9 @@ namespace Cheese_Clicker.Pages
 
             moneyLabelEffect.PlayAnimation(reward);
         }
-
         private void ShopButton_Click(object sender, RoutedEventArgs e)
         {
-            //BounceElement element = new(shopButton);
-            SoundManager.PlaySound(SoundEffects.Click);
-            NavigationService.Navigate(new ShopMenu(player));
+            DisplayShopPage();
         }
 
         private void UpdateStatisticsUI(long moneyGained)
@@ -65,8 +96,7 @@ namespace Cheese_Clicker.Pages
 
         private void AdminButton_Click(object sender, RoutedEventArgs e)
         {
-            SoundManager.PlaySound(SoundEffects.Click);
-            NavigationService.Navigate(new AdminControlPage(player));
+            DisplayAdminPage();
         }
         private void GamePage_Loaded(object sender, RoutedEventArgs e)
         {
@@ -76,19 +106,40 @@ namespace Cheese_Clicker.Pages
 
         private void Inventory_Click(object sender, RoutedEventArgs e)
         {
-            SoundManager.PlaySound(SoundEffects.Click);
-            NavigationService.Navigate(new InventoryPage(player));
+            DisplayInventoryPage();
         }
 
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
-            SoundManager.PlaySound(SoundEffects.Click);
-            NavigationService.Navigate(new SettingsPage());
+            DisplaySettingsPage();
         }
 
         private void UpdateMoneyLabel()
         {
             MoneyLabel.Content = $"Money: ${player.statistics.money:N0}";
+        }
+        private void DisplayShopPage()
+        {
+            SoundManager.PlaySound(SoundEffects.Click);
+            NavigationService.Navigate(new ShopMenu(player));
+        }
+
+        private void DisplayAdminPage()
+        {
+            SoundManager.PlaySound(SoundEffects.Click);
+            NavigationService.Navigate(new AdminControlPage(player));
+        }
+
+        private void DisplayInventoryPage()
+        {
+            SoundManager.PlaySound(SoundEffects.Click);
+            NavigationService.Navigate(new InventoryPage(player));
+        }
+
+        private void DisplaySettingsPage()
+        {
+            SoundManager.PlaySound(SoundEffects.Click);
+            NavigationService.Navigate(new SettingsPage());
         }
     }
 }
